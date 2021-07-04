02c004cc49213af09341fc9dd790f2f2f7085752
sheshe97 <75022318+sheshe97@users.noreply.github.com>
2021-07-04 14:28:51 +0400

    Add files via upload

---
+++ App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+import Todo from "./todo.js"
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+   <Todo />
+      </header>
+    </div>
+  );
+}
+
+export default App;
